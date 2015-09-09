6c368336e94b907db03fd7f48bc30075c7411244
Brian Ng <bcn220@gmail.com>
2015-09-09 12:11:49 -0600

    set up ListContainer component

---
+++ app/ListContainer.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var AddItem = require('./AddItem');
+var List = require('./List');
+
+var ListContainer = React.createClass({
+
+});
+
+module.exports = ListContainer;
